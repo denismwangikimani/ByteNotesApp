cb5947a6b5eae8769eae9c831f939920252e6e50
Denis <deniskimani2022@gmail.com>
2024-08-19 16:10:49 +0300

    finished the frontend part of adding notes

--- notes-app/notes-app/src/App.tsx
+++ notes-app/notes-app/src/App.tsx
@@ -1,15 +1,16 @@
 import "./App.css";
 import { useState } from "react";
 
-interface Notes {
+// Define the type 'Note'
+type Note = {
   id: number;
   title: string;
   content: string;
-}
+};
 
 const App = () => {
   // Create a state variable (6 dummy notes using useState) to store the notes
-  const [notes, setNotes] = useState<Notes[]>([
+  const [notes, setNotes] = useState<Note[]>([
     {
       id: 1,
       title: "Note 1",
@@ -42,14 +43,49 @@ const App = () => {
     },
   ]);
 
+  /*we need to create two state variables to store the title and content of the note*/
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  /*Create a handleAddNote function to handle the form submission.
+  we specify the parameter type as React.FormEvent to satisfy TypeScript's typing requirement*/
+  const handleAddNote = (event: React.FormEvent) => {
+    event.preventDefault(); // Prevent the page from refreshing when submitting the form
+    
+    const newNote: Note = {
+      id: notes.length + 1,
+      title: title,
+      content: content,
+    };
+
+    //we will use the setNotes function to update the notes state variable by adding the new note to the existing notes
+    setNotes([newNote, ...notes]);
+
+    // Clear the input fields after submitting the form
+    setTitle("");
+    setContent("");
+  };
+
   return (
     <div className="app-container">
-      {/* Add a form here containing input field and textarea both are 
+      {/* Add a form (where we will call the handleAddNote under onSubmit) here containing input field and textarea both are 
       required so that the user has to input things and a submit button*/}
-      <form className="note-form">
-        <input placeholder="Title" required />
-        <textarea placeholder="Content" rows={10} required />
+      <form className="note-form" onSubmit={handleAddNote}>
+        <input
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+          placeholder="Title"
+          required
+        ></input>
+        <textarea
+          value={content}
+          onChange={(event) => setContent(event.target.value)}
+          placeholder="Content"
+          rows={10}
+          required
+        ></textarea>
 
+        {/*Add a button to submit the form*/}
         <button type="submit">Add Note</button>
       </form>
 
