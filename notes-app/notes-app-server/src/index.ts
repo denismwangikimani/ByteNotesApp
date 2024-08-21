cbf9457ca89c91a10cb270ee025bbe70a7220d6a
Denis <deniskimani2022@gmail.com>
2024-08-21 10:36:52 +0300

    finished connecting the database

--- notes-app/notes-app-server/src/index.ts
+++ notes-app/notes-app-server/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
+import { PrismaClient } from "@prisma/client";
 
+//create an express app and initiate a Prisma client instance
 const app = express();
+const prisma = new PrismaClient();
 
+//middleware to parse JSON data and enable CORS
 app.use(express.json());
 app.use(cors());
 
+//we will get all notes from the database
 app.get("/api/notes", async (req, res) => {
-  res.json({ message: "success!" });
+  const notes = await prisma.note.findMany();
+  res.json({ notes });
 });
 
 app.listen(5000, () => {
